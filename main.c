957bcf9999b37857f33f8a84b9248236afc7ecc4
Emil Lengman <emillen@kth.se>
2016-05-18 19:24:59 +0200

    removing debug prints and changed indentation

--- main.c
+++ main.c
@@ -18,69 +18,67 @@ void qTelldus();
 
 void WINAPI sensorEvent(const char *protocol, const char *model, int sensorId, int dataType, const char *value, int ts, int callbackId, void *context) {
     //prints when not equal to  90, so not cool
-	if(sensorId != 187)
-		return;	
-
-    	if(strcmp("90.0",value) && !comingFromCallback){
-		comingFromCallback = 1;
-
-		paused = 1;
-		//tdUnregisterCallback( callbackId );
-		if(getLastTriggerFromSQL() == 1){
-			printf("triggered\n");
-			sendTriggerToSQL();
-			triggerdLoop();
-		}
-		paused = 0;
+    if(sensorId != 187)
+	return;	
+    
+    if(strcmp("90.0",value) && !comingFromCallback){
+	comingFromCallback = 1;
+	
+	paused = 1;
+	//tdUnregisterCallback( callbackId );
+	if(getLastTriggerFromSQL() == 1){
+	    sendTriggerToSQL();
+	    triggerdLoop();
 	}
-	// prints when not -27, soo... cool
-    	else if(strcmp("-27.0", value) && !comingFromCallback){
-		sinceLast = 0;
-       	    	printf("is cool \n");
-	}
+	paused = 0;
+    }
+    // prints when not -27, soo... cool
+    else if(strcmp("-27.0", value) && !comingFromCallback){
+	sinceLast = 0;
+	printf("is cool \n");
+    }
 }
 
 void registerTelldus(){
-	tdInit();
-        callbackId = tdRegisterSensorEvent((TDSensorEvent)&sensorEvent, 0);
+    tdInit();
+    callbackId = tdRegisterSensorEvent((TDSensorEvent)&sensorEvent, 0);
 }
 
 void qTelldus(){
-	tdUnregisterCallback(callbackId);
-	tdClose();
+    tdUnregisterCallback(callbackId);
+    tdClose();
 }
 
 int main(void) {
-	printf("starting up...\n");
-	createConnectionSQL();
-
-	//Register for callback
-	registerTelldus();
-	printf("Done \n");
-	//Our own simple eventloop
-
-	while(1) {
-		if(!paused){
-			sinceLast+=1;
-			if(comingFromCallback){
-				sinceLast = 0;
-				comingFromCallback = 0;
-				printf("Im here fgt boii\n");
-			}
-			if(sinceLast > 5){
-				sendTriggerToSQL();
-				triggerdLoop();
-			}
-		}
-		sleep(1);
+    printf("starting up...\n");
+    createConnectionSQL();
+    
+    //Register for callback
+    registerTelldus();
+    printf("Done \n");
+    //Our own simple eventloop
+    
+    while(1) {
+	if(!paused){
+	    sinceLast+=1;
+	    if(comingFromCallback){
+		sinceLast = 0;
+		comingFromCallback = 0;
+	    }
+	    if(sinceLast > 5){
+		sendTriggerToSQL();
+		triggerdLoop();
+	    }
 	}
-
-
-	//Cleanup
-
-	qTelldus();
-	mysql_close(connection);
-
-	return 0;
+	sleep(1);
+    }
+    
+    
+    //Cleanup
+    
+    qTelldus();
+    mysql_close(connection);
+    
+    return 0;
 }
 
